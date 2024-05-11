fe0dd1b65b8d5240c15a520817b7189137cc5478
Apurba Sarkar <sarkar.apurba@hotmail.com>
2024-05-11 19:26:04 +0530

    sd

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import "./Global/variable.css";
+
+function App() {
+
+  return (
+    <>
+      <div style={{ color: "var(--yellow-orange)" }}>dsad</div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Global/global.js
@@ -0,0 +1,9 @@
+const colors = {
+    primaryColor: '#007bff',
+    secondaryColor: '#6c757d',
+    textColor: '#333',
+    backgroundColor: '#fff',
+    
+  };
+  
+  export default colors;
